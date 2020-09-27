d8df589a9fecb9d906fe6211f3904dee2ec22824
CJ Gilroy <cjgilroy@MacBook-Pro.local>
2020-09-27 16:25:21 -0600

    Adding user and artwork associations

--- app/models/artwork.rb
+++ app/models/artwork.rb
@@ -3,4 +3,6 @@ class Artwork < ActiveRecord
     validates :title, :image_url, presence:true
     validates :image_url, uniqueness:true
     validates :title, uniqueness: { scope: :artist_id }
+
+    belongs_to :artist, primary_key: :id, foreign_key: :artist_id, class_name: 'User'
 end
