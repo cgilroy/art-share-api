7ef8ec68360a6dab29a4290100c99fb40a8e5790
CJ Gilroy <cjgilroy@MacBook-Pro.local>
2020-09-27 16:12:27 -0600

    Create artworks table/model

---
+++ app/models/artwork.rb
@@ -0,0 +1,6 @@
+
+class Artwork < ActiveRecord
+    validates :title, :image_url, presence:true
+    validates :image_url, uniqueness:true
+    validates :title, uniqueness: { scope: :artist_id }
+end
