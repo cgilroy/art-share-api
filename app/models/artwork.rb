5ea28f2f99cf850668c4a59f1a452e96c7f70dd9
CJ Gilroy <c.gilroy9@gmail.com>
2020-09-28 15:14:50 -0600

    Add ArtworkShares an associations

--- app/models/artwork.rb
+++ app/models/artwork.rb
@@ -1,8 +1,12 @@
 
-class Artwork < ActiveRecord
+class Artwork < ApplicationRecord
     validates :title, :image_url, presence:true
     validates :image_url, uniqueness:true
     validates :title, uniqueness: { scope: :artist_id }
 
     belongs_to :artist, primary_key: :id, foreign_key: :artist_id, class_name: 'User'
+    has_many :artwork_shares
+    has_many :shared_viewers,
+        through: :artwork_shares,
+        source: :viewer
 end
